b25b85318449b56376bd350aabfe3a335fa49685
Tiago Sathler <sathler@gmail.com>
2023-10-17 07:50:40 -0300

    :sparkles: feat: create Position class

---
+++ ChessConsole/Board/Position.cs
@@ -0,0 +1,28 @@
+﻿namespace ChessConsole.Board;
+
+internal sealed class Position
+{
+    public int Row { get; set; }
+    public int Column { get; set; }
+
+    public Position()
+    {
+    }
+
+    public Position(int row, int column)
+    {
+        Row = row;
+        Column = column;
+    }
+
+    public void SetValues(int row, int column)
+    {
+        Row = row;
+        Column = column;
+    }
+
+    public override string ToString()
+    {
+        return $"{Row}, {Column}";
+    }
+}
