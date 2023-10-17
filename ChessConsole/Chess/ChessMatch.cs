f2cfaf54321837661f9d6b44068c2671c2822deb
Tiago Sathler <sathler@gmail.com>
2023-10-17 09:27:28 -0300

    :sparkles: feat: adds ChessMatch and ChessPiece classes

---
+++ ChessConsole/Chess/ChessMatch.cs
@@ -0,0 +1,34 @@
+﻿using ChessConsole.Boardgame;
+
+namespace ChessConsole.Chess;
+
+internal sealed class ChessMatch
+
+{
+    public static readonly int BOARD_SIZE = 8;
+
+    public int Turn { get; set; }
+    public Color CurrentPlayer { get; set; }
+
+    private readonly Board board;
+
+    public ChessMatch()
+    {
+        board = new Board(BOARD_SIZE, BOARD_SIZE);
+    }
+
+    public ChessPiece[,] GetPieces()
+    {
+        ChessPiece[,] chessPieces = new ChessPiece[board.Rows, board.Columns];
+
+        for (int i = 0; i < BOARD_SIZE; i++)
+        {
+            for (int j = 0; j < BOARD_SIZE; j++)
+            {
+                chessPieces[i, j] = (ChessPiece)board.Piece(i, j);
+            }
+        }
+
+        return chessPieces;
+    }
+}
---
+++ ChessConsole/Chess/ChessPiece.cs
@@ -0,0 +1,29 @@
+﻿using ChessConsole.Boardgame;
+
+namespace ChessConsole.Chess;
+
+internal abstract class ChessPiece : Piece
+{
+    public Color Color { get; }
+    public int MoveCount { get; private set; }
+
+    protected ChessPiece(Board board, Color color) : base(board)
+    {
+        Color = color;
+    }
+
+    protected bool IsThereOpponentPiece(Position position)
+    {
+        throw new NotImplementedException();
+    }
+
+    protected void IncreaseMoveCount()
+    {
+        MoveCount++;
+    }
+
+    protected void DecreaseMoveCount()
+    {
+        MoveCount--;
+    }
+}
