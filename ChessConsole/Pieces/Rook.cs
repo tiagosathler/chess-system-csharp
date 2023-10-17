1fbb8495eb36a13dbd8f7ecb0229b3b5be5bda37
Tiago Sathler <sathler@gmail.com>
2023-10-17 16:36:04 -0300

    :sparkles: feat: implements the PossilbeMoves method on the Rook piece

--- ChessConsole/Pieces/Rook.cs
+++ ChessConsole/Pieces/Rook.cs
@@ -11,7 +11,59 @@ internal sealed class Rook : ChessPiece
 
     protected override bool[,] PossibleMoves()
     {
-        throw new NotImplementedException();
+        bool[,] mat = new bool[Board.BOARD_SIZE, Board.BOARD_SIZE];
+
+        Position p = new(0, 0);
+
+        // above:
+        p.SetValues(Position!.Row - 1, Position!.Column);
+        while (Board.PositionExists(p) && !Board.IsThereAPiece(p))
+        {
+            mat[p.Row, p.Column] = true;
+            p.Row--;
+        }
+        if (Board.PositionExists(p) && ThereIsOpponentPiece(p))
+        {
+            mat[p.Row, p.Column] = true;
+        }
+
+        // left:
+        p.SetValues(Position!.Row, Position!.Column - 1);
+        while (Board.PositionExists(p) && !Board.IsThereAPiece(p))
+        {
+            mat[p.Row, p.Column] = true;
+            p.Column--;
+        }
+        if (Board.PositionExists(p) && ThereIsOpponentPiece(p))
+        {
+            mat[p.Row, p.Column] = true;
+        }
+
+        // left:
+        p.SetValues(Position!.Row, Position!.Column + 1);
+        while (Board.PositionExists(p) && !Board.IsThereAPiece(p))
+        {
+            mat[p.Row, p.Column] = true;
+            p.Column++;
+        }
+        if (Board.PositionExists(p) && ThereIsOpponentPiece(p))
+        {
+            mat[p.Row, p.Column] = true;
+        }
+
+        // bellow:
+        p.SetValues(Position.Row + 1, Position.Column);
+        while (Board.PositionExists(p) && !Board.IsThereAPiece(p))
+        {
+            mat[p.Row, p.Column] = true;
+            p.Row++;
+        }
+        if (Board.PositionExists(p) && ThereIsOpponentPiece(p))
+        {
+            mat[p.Row, p.Column] = true;
+        }
+
+        return mat;
     }
 
     public override string ToString()
