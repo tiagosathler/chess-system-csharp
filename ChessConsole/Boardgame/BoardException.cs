4acb5fcbebe378ff5a024098a2a36fcac0ad4c70
Tiago Sathler <sathler@gmail.com>
2023-10-17 11:46:09 -0300

    :sparkles: feat: adds BoardException class

---
+++ ChessConsole/Boardgame/BoardException.cs
@@ -0,0 +1,16 @@
+﻿namespace ChessConsole.Boardgame;
+
+public class BoardException : ApplicationException
+{
+    public BoardException()
+    {
+    }
+
+    public BoardException(string? message) : base(message)
+    {
+    }
+
+    public BoardException(string? message, Exception? innerException) : base(message, innerException)
+    {
+    }
+}
