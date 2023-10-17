72cd7e6c71d19fc8c7653495531fe700d620d3fb
Tiago Sathler <sathler@gmail.com>
2023-10-17 10:09:23 -0300

    :sparkles: feat: adds King and Rooks ChessPiece classes

---
+++ ChessConsole/Pieces/King.cs
@@ -0,0 +1,21 @@
+﻿using ChessConsole.Boardgame;
+using ChessConsole.Chess;
+
+namespace ChessConsole.Pieces;
+
+internal sealed class King : ChessPiece
+{
+    public King(Board board, Color color) : base(board, color)
+    {
+    }
+
+    protected override bool[,] PossibleMoves()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override string ToString()
+    {
+        return "K";
+    }
+}
---
+++ ChessConsole/Pieces/Rook.cs
@@ -0,0 +1,21 @@
+﻿using ChessConsole.Boardgame;
+using ChessConsole.Chess;
+
+namespace ChessConsole.Pieces;
+
+internal sealed class Rook : ChessPiece
+{
+    public Rook(Board board, Color color) : base(board, color)
+    {
+    }
+
+    protected override bool[,] PossibleMoves()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override string ToString()
+    {
+        return "R";
+    }
+}
